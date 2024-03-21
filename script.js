f03b31dae434b87994dfbda37366ed9e3051ad4c
Divya Alex <divyamalex@gmail.com>
2024-03-21 16:39:03 -0400

    +Books page

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ btnPopup.addEventListener('click', ()=> {
 iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
 });
+
+let imgBx = document.querySelectorAll('.imgBx');
+let contentBx = document.querySelectorAll('.contentBx');
+
+for(var i = 0; i < imgBx.length; i++) {
+    imgBx[i].addEventListener('mouseover', function(){
+        for(var i = 0; i < contentBx.length; i++) {
+            contentBx[i].className = 'contentBx';
+        }
+        document.getElementById(this.dataset.id).className = 'contentBx active';
+        for(var i = 0; i < imgBx.length; i++) {
+            imgBx[i].className = 'imgBx';
+        }
+        this.className = 'imgBx active';
+    })
+}
